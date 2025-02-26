276785833304721e737ec2f9bca6e3017907d7a1
Eoghan West <eowest@gmail.com>
2025-02-26 03:12:48 -0500

    backup commit

--- src/synth_engines/synth/osc.rs
+++ src/synth_engines/synth/osc.rs
@@ -111,6 +111,11 @@ impl Oscillator {
         // }
         self.detune();
 
+        info!(
+            "modulated volume {}",
+            calculate_modulation(self.level, self.level_mod)
+        );
+
         self.osc.get_sample(&self.wave_table) * calculate_modulation(self.level, self.level_mod)
     }
 
